727f3d5c6bac9aff87edb1ea7bd92647cf7f1152
Egor Potemkin <e_potemkin@irknet.ru>
2015-12-16 19:20:16 +0800

    [master]: fix lint warnings

--- .vagrant_puppet/init.pp
+++ .vagrant_puppet/init.pp
@@ -2,19 +2,18 @@ include mzteststart
 # machinzone test task
 # install and configure smokeping
 class mzteststart {
-  # variables
   # https://en.wikipedia.org/wiki/List_of_most_popular_websites
   class { 'mztest':
     ssites => {
-          'Google' => 'google.com',
-          'Facebook' => 'facebook.com',
-          'Youtube' => 'youtube.com',
-          'Baidu' => 'baidu.com',
-          'Yahoo' => 'yahoo.com',
-          'Amazon' => 'amazon.com',
-          'Wikipedia' => 'wikipedia.org',
-          'TencentQQ' => 'qq.com',
-          'Twitter' => 'twitter.com',
+      'Google'    => 'google.com',
+      'Facebook'  => 'facebook.com',
+      'Youtube'   => 'youtube.com',
+      'Baidu'     => 'baidu.com',
+      'Yahoo'     => 'yahoo.com',
+      'Amazon'    => 'amazon.com',
+      'Wikipedia' => 'wikipedia.org',
+      'TencentQQ' => 'qq.com',
+      'Twitter'   => 'twitter.com',
     }
   }
 }
